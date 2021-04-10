2509cd1c0356f7ef1eb7facb71e75f4e6fa80095
Patrick Smith <81268345+pdsmith90@users.noreply.github.com>
2021-04-10 10:14:18 -0400

    Create bill_division.py

---
+++ practice/problem_solving/bill_division.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the bonAppetit function below.
+def bonAppetit(bill, k, b):
+    del bill[k]
+    realCost=sum(bill)/2
+    if realCost==b: print("Bon Appetit")
+    else: print(round(b-realCost))
+
+if __name__ == '__main__':
+    nk = input().rstrip().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    bill = list(map(int, input().rstrip().split()))
+
+    b = int(input().strip())
+
+    bonAppetit(bill, k, b)
