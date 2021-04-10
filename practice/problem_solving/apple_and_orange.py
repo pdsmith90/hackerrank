877797334f628b61939e2afb111b12506100e7b6
Patrick Smith <81268345+pdsmith90@users.noreply.github.com>
2021-04-10 10:11:13 -0400

    Create apple_and_orange.py

---
+++ practice/problem_solving/apple_and_orange.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countApplesAndOranges function below.
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    applesLanded=orangesLanded=0
+    for apple in apples:
+        if s<=a+apple<=t:
+            applesLanded+=1
+        
+    for orange in oranges:
+        if s<=b+orange<=t:
+            orangesLanded+=1
+    print(applesLanded)
+    print(orangesLanded)
+    
+    
+if __name__ == '__main__':
+    st = input().split()
+
+    s = int(st[0])
+
+    t = int(st[1])
+
+    ab = input().split()
+
+    a = int(ab[0])
+
+    b = int(ab[1])
+
+    mn = input().split()
+
+    m = int(mn[0])
+
+    n = int(mn[1])
+
+    apples = list(map(int, input().rstrip().split()))
+
+    oranges = list(map(int, input().rstrip().split()))
+
+    countApplesAndOranges(s, t, a, b, apples, oranges)
