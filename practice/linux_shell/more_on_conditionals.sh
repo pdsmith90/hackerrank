328b9a2ee7fbd6db52c6ac8ff80ed78ccca822ab
Patrick Smith <81268345+pdsmith90@users.noreply.github.com>
2021-04-12 09:32:35 -0400

    Update more_on_conditionals.sh

--- practice/linux_shell/more_on_conditionals.sh
+++ practice/linux_shell/more_on_conditionals.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+read A
+read B
+read C
+
+if [ "$A" = "$B" ] && [ "$A" = "$C" ]; then
+    echo "EQUILATERAL"
+elif [ "$A" = "$B" ] || [ "$A" = "$C" ]; then
+    echo "ISOSCELES"
+else
+    echo "SCALENE"
+fi
